25e78f8a1cb1f219e42a4b3d628367974cf15f3d
lsflp <lipeluiis@hotmail.com>
2016-10-18 19:22:49 -0200

    Começando

---
+++ compress.m
@@ -0,0 +1,10 @@
+% Nomes: Luís Felipe de M. C. Silva    Nº USP: 9297961
+%        Nícolas Nogueira L. da Silva          9277541
+%
+% Arquivo parte do EP2 de MAC0210
+
+function compress (originalImg, k)
+    x = imread (originalImg);
+    s = size(x);
+    for i = 1: :
+endfunction
