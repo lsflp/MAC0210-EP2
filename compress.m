bff7b7b589250d14786e2651076bac1058300600
lsflp <lipeluiis@hotmail.com>
2016-10-18 20:55:55 -0200

    Tá bugado :(

--- compress.m
+++ compress.m
@@ -4,7 +4,21 @@
 % Arquivo parte do EP2 de MAC0210
 
 function compress (originalImg, k)
-    x = imread (originalImg);
-    s = size(x);
-    for i = 1: :
+    I = imread (originalImg);
+    s = size(I);
+    n = floor((s(1)+k)/(1+k));
+    J = zeros(n, n, 3);
+    
+    for i = 1:s(1)
+        for j = 1:s(1)
+            if (rem(i, k+1) == 1 && rem(j, k+1) == 1)
+                J(floor(i/k)+1, floor(j/k)+1, 1) = I(i, j, 1);
+                J(floor(i/k)+1, floor(j/k)+1, 2) = I(i, j, 2);
+                J(floor(i/k)+1, floor(j/k)+1, 3) = I(i, j, 3);
+            endif
+        endfor
+    endfor
+    
+    imwrite (J, "compressed.png");
+    
 endfunction
