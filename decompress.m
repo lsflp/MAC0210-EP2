70c0c960e8868eadccb081cddf2ec7264fe70261
lsflp <lipeluiis@hotmail.com>
2016-10-24 15:38:48 -0200

    A matriz é expandida só faltainterpolar os pontos

--- decompress.m
+++ decompress.m
@@ -3,20 +3,37 @@
 %
 % Arquivo parte do EP2 de MAC0210
 
-function decompress (compressedImg, method, k) 
+function decompress (compressedImg, method, k, h, x_, y_) 
 	A = imread (compressedImg);
 	if method == 1
-		B = bilinear (A, k);
+		B = bilinear (A, k, h, x_, y_);
 	else
-		B = bicubico (A, k);
+		B = bicubico (A, k, h, x_, y_);
 	endif
-	imwrite ('decompressed.png');
+	imwrite (B, 'decompressed.png');
 endfunction
 
-function B = bilinear (A, k)
-
+function B = bilinear (A, k, h, x_, y_)
+	B = expande (A, k);	
 endfunction
 
-funcion B = bicubico (A, k)
+function B = bicubico (A, k, h, x_, y_)
+	B = expande (A, k);
+endfunction
+
+% Expande uma matriz quadrada de lado n para uma de lado n+(n-1)*k
+function B = expande (A, k)
+	n = size(A)(1);
+	p = n+(n-1)*k;
+
+	for i = 1:p
+        for j = 1:p
+           if rem(i, k+1) == 1 && rem(j, k+1) == 1
+           		B(i, j, :) = A(((i-1)/(k+1))+1, ((j-1)/(k+1))+1, :);
+           else
+           		B(i, j, :) = 0;
+           endif 
+        endfor
+    endfor
 
 endfunction
