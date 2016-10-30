ce0eee7732db7ccc8733e58d35176c4f67e46875
Nicolas Nogueira <nicolas.nogueira@hotmail.com>
2016-10-29 22:08:40 -0200

    base cubico

--- decompress.m
+++ decompress.m
@@ -57,6 +57,51 @@ endfunction
 % tamanho p. O k e o h são os mesmos da função decompress.
 function B = bicubico (A, k, h, p)
     B = expande (A, k, p);
+    % No enunciado essa é a matriz B
+    C = [1, 0, 0, 0; 0, 0, 1, 0; -3./(h.^2), 3./(h.^2), -2./h, -1./h; 2./(h.^3), -2./(h.^3), 1./(h.^2), 1./(h.^2)];
+    % Para cada quadrado.
+    for i = 1:k+1:p-(k+1)
+        for j = 1:k+1:p-(k+1)
+            % Pega os valores dos pixels que estão nos extremos dos quadrados.
+            fx1y1 = B(i, j, :);
+            fx1y2 = B(i, j+(k+1), :);
+            fx2y1 = B(i+(k+1), j, :);
+            fx2y2 = B(i+(k+1), j+(k+1), :);
+            dyfx1y1 = ?;
+            dyfx1y2 = ?;
+            dyfx2y1 = ?;
+            dyfx2y2 = ?;
+            dxfx1y1 = ?;
+            dxfx1y2 = ?;
+            dxfx2y1 = ?;
+            dxfx2y2 = ?;
+            dxyfx1y1 = ?;
+            dxyfx1y2 = ?;
+            dxyfx2y1 = ?;
+            dxyfx2y2 = ?;
+            % Faz a interpolação para cada cor, na ordem vermelho, verde e azul.
+            for l = 1:3
+                D =   [  fx1y1(l),   fx1y2(l),  dyfx1y1(l),  dyfx1y2(l); 
+                         fx2y1(l),   fx2y2(l),  dyfx2y1(l),  dyfx2y2(l);
+                       dxfx1y1(l), dxfx1y2(l), dxyfx1y1(l), dxyfx1y2(l);
+                       dxfx2y1(l), dxfx2y2(l), dxyfx2y1(l), dxyfx2y2(l)];
+                E = double(D);
+                % Matriz de coeficientes do polinomio intepolador
+                X = C*E*transpose(C);
+                % Para cada pixel dentro desse quadrado.
+                for m = i:i+k+1
+                    for n = j:j+k+1
+                        % Se não for o pixel original usado para a interpolação.
+                        if (m != i && n != j) 
+                            x = ((m-i)/(k+1))*h;
+                            y = ((n-j)/(k+1))*h;
+                            B(m, n, l) = [1, x, x.^2, x.^3]*X*[1; y; y.^2; y.^3];
+                        endif
+                    endfor
+                endfor
+            endfor
+        endfor
+    endfor
 endfunction
 
 % Expande uma matriz quadrada de lado n para uma de lado n+(n-1)*k (De 
